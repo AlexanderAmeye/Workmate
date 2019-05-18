1fabf4cc15d19e896b53a9bf82e00bf89d589f35
Alexander Ameye <alexanderameye@gmail.com>
2019-05-18 13:10:32 +0200

    Initial Commit

--- app/src/main/java/me/example/paul/Activities/RewardsActivity.java
+++ app/src/main/java/me/example/paul/Activities/RewardsActivity.java
@@ -1,31 +1,28 @@
 package me.example.paul.Activities;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.GridView;
 
-import me.example.paul.Model.Reward;
+import me.example.paul.CardFragmentPagerAdapter;
 import me.example.paul.R;
-import me.example.paul.RewardsAdapter;
+import me.example.paul.ShadowTransformer;
 
 public class RewardsActivity extends AppCompatActivity {
 
 
-    private Reward[] rewards = {
-            new Reward("Reward1", "Description 1", R.drawable.areyoumymother),
-
-            new Reward("Reward2", "Description 2", R.drawable.theveryhungrycaterpillar)
-
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reward_store);
+        setContentView(R.layout.activity_store);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
 
-        GridView gridView = (GridView)findViewById(R.id.gridview);
-        RewardsAdapter rewardsAdapter = new RewardsAdapter(this, rewards);
-        gridView.setAdapter(rewardsAdapter);
+        CardFragmentPagerAdapter pagerAdapter = new CardFragmentPagerAdapter(getSupportFragmentManager(), 50);
+        ShadowTransformer fragmentCardShadowTransformer = new ShadowTransformer(viewPager, pagerAdapter);
+        fragmentCardShadowTransformer.enableScaling(true);
 
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setPageTransformer(false, fragmentCardShadowTransformer);
+        viewPager.setOffscreenPageLimit(3);
     }
 }
