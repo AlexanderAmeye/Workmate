c5873594e8a141fe1632bb90579e8057aa2cf99d
Alexander Ameye <alexanderameye@gmail.com>
2019-05-19 19:49:56 +0200

    Initial Commit

--- app/src/main/java/me/example/paul/Activities/LoginActivity.java
+++ app/src/main/java/me/example/paul/Activities/LoginActivity.java
@@ -3,19 +3,15 @@ package me.example.paul.Activities;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,92 +20,74 @@ import me.example.paul.R;
 import me.example.paul.SessionManager;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText emailLoginText, passwordLoginText;
-    private ProgressDialog progressDialog;
 
+    private RequestQueue serverQueue;
     SessionManager sessionManager;
 
-    private String getUserURL = "https://studev.groept.be/api/a18_sd308/GetUser/";
-    private RequestQueue serverQueue;
+    private EditText emailField, passwordField;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //System
+        serverQueue = Volley.newRequestQueue(this);
         sessionManager = new SessionManager(this);
 
-        emailLoginText = findViewById(R.id.usernameFieldText);
-        passwordLoginText = findViewById(R.id.emailRegisterText);
+        //UI
+        emailField = findViewById(R.id.usernameField);
+        passwordField = findViewById(R.id.emailField);
         progressDialog = new ProgressDialog(this);
 
-        Button logInButton = findViewById(R.id.registerButton);
-        logInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LoginUser();
-            }
-        });
-
-        serverQueue = Volley.newRequestQueue(this);
+        Button loginButton = findViewById(R.id.registerButton);
+        loginButton.setOnClickListener(v -> Login());
     }
 
-    @Override
-    protected void onStart() {  //called whenever application becomes visible
-        super.onStart();
-    }
-
-    public void LoginUser() {
-        final String email = emailLoginText.getText().toString().trim();
-        final String password = passwordLoginText.getText().toString().trim();
+    public void Login() {
+        final String email = emailField.getText().toString().trim();
+        final String password = passwordField.getText().toString().trim();
 
         if (email.isEmpty() || password.isEmpty()) {
-            Toast.makeText(LoginActivity.this, "Some fields were left empty", Toast.LENGTH_SHORT).show();
+            Toast.makeText(LoginActivity.this, "Some fields were left blank", Toast.LENGTH_SHORT).show();
         } else {
             progressDialog.setMessage("Login in progress");
             progressDialog.show();
 
-            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, getUserURL + email, null, new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-                    JSONArray array = response;
-                    if (array.length() != 0) //email was found
-                    {
-                        try {
-                            JSONObject object = array.getJSONObject(0);
-                            String found_password = object.get("password").toString();
-                            String found_username = object.get("username").toString();
-                            String found_email = object.get("email").toString();
+            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, "https://studev.groept.be/api/a18_sd308/GetUser/" + email, null, response -> {
+                if (response.length() != 0) { //user was found for given email
+                    try {
+                        JSONObject object = response.getJSONObject(0);
+                        String found_password = object.get("password").toString();
+                        String found_username = object.get("username").toString();
+                        String found_email = object.get("email").toString();
 
-                            if (AESCrypt.decrypt(found_password).equals(password)) //correct password
-                            {
-                                Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
-                                progressDialog.dismiss();
-                                sessionManager.createSession(found_username, found_email);
-                            } else {
-                                Toast.makeText(LoginActivity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
-                                progressDialog.dismiss();
-                            }
-                        } catch (JSONException e) {
-                            Toast.makeText(LoginActivity.this, "Failed to get data from server", Toast.LENGTH_SHORT).show();
+                        if (AESCrypt.decrypt(found_password).equals(password)) //correct password
+                        {
+                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
                             progressDialog.dismiss();
-                            e.printStackTrace();
-                        } catch (Exception e) {
-                            Toast.makeText(LoginActivity.this, "Failed to decrypt password", Toast.LENGTH_SHORT).show();
+                            sessionManager.createSession(found_username, found_email);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
                             progressDialog.dismiss();
-                            e.printStackTrace();
                         }
-                    } else {
-                        Toast.makeText(LoginActivity.this, "Email not recognised", Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        Toast.makeText(LoginActivity.this, "Failed to get data from server", Toast.LENGTH_SHORT).show();
                         progressDialog.dismiss();
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        Toast.makeText(LoginActivity.this, "Failed to decrypt password", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                        e.printStackTrace();
                     }
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Email not recognised", Toast.LENGTH_SHORT).show();
                     progressDialog.dismiss();
                 }
+            }, error -> {
+                Toast.makeText(LoginActivity.this, "Connection error", Toast.LENGTH_LONG).show();
+                progressDialog.dismiss();
             }
             );
             serverQueue.add(request);
--- app/src/main/java/me/example/paul/Activities/RegisterActivity.java
+++ app/src/main/java/me/example/paul/Activities/RegisterActivity.java
@@ -6,20 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Patterns;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,173 +24,146 @@ import me.example.paul.R;
 import me.example.paul.SessionManager;
 
 public class RegisterActivity extends AppCompatActivity {
-    private EditText usernameFieldText, emailFieldText, passwordFieldText, passwordConfirmationFieldText;
-    private ProgressDialog progressDialog;
 
+    private RequestQueue serverQueue;
     SessionManager sessionManager;
 
-    private String registerUserUrl = "https://studev.groept.be/api/a18_sd308/AddUser/";
-    private String getUserUrl = "https://studev.groept.be/api/a18_sd308/GetUser/";
-    private RequestQueue serverQueue;
+    private EditText usernameField, emailField, passwordField, passwordConfirmationField;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        //System
+        serverQueue = Volley.newRequestQueue(this);
         sessionManager = new SessionManager(this);
 
-        usernameFieldText = findViewById(R.id.usernameFieldText);
-        emailFieldText = findViewById(R.id.emailRegisterText);
-        passwordFieldText = findViewById(R.id.passwordRegisterText);
-        passwordConfirmationFieldText = findViewById(R.id.passwordConfirmationRegisterText);
+        //UI
+        usernameField = findViewById(R.id.usernameField);
+        emailField = findViewById(R.id.emailField);
+        passwordField = findViewById(R.id.passwordField);
+        passwordConfirmationField = findViewById(R.id.passwordConfirmationField);
         progressDialog = new ProgressDialog(this);
 
         final Button registerButton = findViewById(R.id.registerButton);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RegisterUser();
-            }
-        });
-        serverQueue = Volley.newRequestQueue(this);
+        registerButton.setOnClickListener(v -> AttemptRegistration());
 
-
-        emailFieldText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                String email = emailFieldText.getText().toString().trim();
-                if (!isValidEmail(email)) {
-                    emailFieldText.setBackgroundResource(R.drawable.input_box_incorrect);
-                } else {
-                    emailFieldText.setBackgroundResource(R.drawable.input_box_correct);
-                }
-            }
-        });
-
-        passwordFieldText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                String password = passwordFieldText.getText().toString().trim();
-
-                if (password.length() < 6) {
-                    passwordFieldText.setBackgroundResource(R.drawable.input_box_incorrect);
-                } else {
-                    passwordFieldText.setBackgroundResource(R.drawable.input_box_correct);
-                }
-            }
-        });
-
-        passwordConfirmationFieldText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                String password = passwordFieldText.getText().toString().trim();
-                String passwordConfirmation = passwordConfirmationFieldText.getText().toString().trim();
-
-                if (!password.equals(passwordConfirmation)) {
-                    passwordConfirmationFieldText.setBackgroundResource(R.drawable.input_box_incorrect);
-                } else {
-                    passwordConfirmationFieldText.setBackgroundResource(R.drawable.input_box_correct);
-                }
-            }
-        });
+        emailField.addTextChangedListener(emailFieldTextWatcher);
+        passwordField.addTextChangedListener(passwordFieldTextWatcher);
+        passwordConfirmationField.addTextChangedListener(passwordConfirmationFieldTextWatcher);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+    TextWatcher emailFieldTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
 
-    public void RegisterUser() {
-        final String username = usernameFieldText.getText().toString().trim();
-        final String email = emailFieldText.getText().toString().trim();
-        final String password = passwordFieldText.getText().toString().trim();
-        final String passwordConfirmation = passwordConfirmationFieldText.getText().toString().trim();
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String email = emailField.getText().toString().trim();
+            if (incorrectEmail(email)) {
+                emailField.setBackgroundResource(R.drawable.input_box_incorrect);
+            } else {
+                emailField.setBackgroundResource(R.drawable.input_box_correct);
+            }
+        }
+    };
+
+    TextWatcher passwordFieldTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String password = passwordField.getText().toString().trim();
+
+            if (password.length() < 6) {
+                passwordField.setBackgroundResource(R.drawable.input_box_incorrect);
+            } else {
+                passwordField.setBackgroundResource(R.drawable.input_box_correct);
+            }
+        }
+    };
+
+    TextWatcher passwordConfirmationFieldTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            String password = passwordField.getText().toString().trim();
+            String passwordConfirmation = passwordConfirmationField.getText().toString().trim();
+
+            if (!password.equals(passwordConfirmation)) {
+                passwordConfirmationField.setBackgroundResource(R.drawable.input_box_incorrect);
+            } else {
+                passwordConfirmationField.setBackgroundResource(R.drawable.input_box_correct);
+            }
+        }
+    };
+
+    public void AttemptRegistration() {
+        final String username = usernameField.getText().toString().trim();
+        final String email = emailField.getText().toString().trim();
+        final String password = passwordField.getText().toString().trim();
+        final String passwordConfirmation = passwordConfirmationField.getText().toString().trim();
 
         if (username.isEmpty() || email.isEmpty() || password.isEmpty() || passwordConfirmation.isEmpty()) {
-            Toast.makeText(RegisterActivity.this, "Some fields were left empty", Toast.LENGTH_SHORT).show();
+            Toast.makeText(RegisterActivity.this, "Some fields were left blank", Toast.LENGTH_SHORT).show();
         } else if (!password.equals(passwordConfirmation)) {
             Toast.makeText(RegisterActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
-            passwordConfirmationFieldText.requestFocus();
+            passwordConfirmationField.requestFocus();
         } else if (password.length() < 6) {
             Toast.makeText(RegisterActivity.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
-            passwordFieldText.requestFocus();
+            passwordField.requestFocus();
 
-        } else if (!isValidEmail(email)) {
+        } else if (incorrectEmail(email)) {
             Toast.makeText(RegisterActivity.this, "Invalid email", Toast.LENGTH_SHORT).show();
         } else {
-            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, getUserUrl + email, null, new Response.Listener<JSONArray>() {
-                @Override
-                public void onResponse(JSONArray response) {
-                    if (response.length() > 0) {
-                        Toast.makeText(RegisterActivity.this, "Account with email already exists", Toast.LENGTH_SHORT).show();
-                    } else {
-                        try {
-                            Register(username, password, email);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, "https://studev.groept.be/api/a18_sd308/GetUser/" + email, null, response -> {
+                if (response.length() > 0) {
+                    Toast.makeText(RegisterActivity.this, "An account with that email already exists", Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        Register(username, password, email);
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                }
+            }, error -> {
             });
             serverQueue.add(request);
         }
     }
 
     public void Register(final String username, final String password, final String email) throws Exception {
-
         progressDialog.setMessage("Registration in progress");
         progressDialog.show();
 
-        String encryptedPassword = AESCrypt.encrypt(password);
-
-        StringRequest request = new StringRequest(Request.Method.POST, registerUserUrl + username + "/" + email + "/" + encryptedPassword, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Toast.makeText(RegisterActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
-                progressDialog.dismiss();
-                sessionManager.createSession(username, email);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(RegisterActivity.this, "Registration failed", Toast.LENGTH_LONG).show();
-                progressDialog.dismiss();
-            }
+        String registerUserUrl = "https://studev.groept.be/api/a18_sd308/AddUser/";
+        StringRequest request = new StringRequest(Request.Method.POST, registerUserUrl + username + "/" + email + "/" + AESCrypt.encrypt(password), response -> {
+            Toast.makeText(RegisterActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();
+            sessionManager.createSession(username, email);
+        }, error -> {
+            Toast.makeText(RegisterActivity.this, "Registration failed", Toast.LENGTH_LONG).show();
+            progressDialog.dismiss();
         }) {
             @Override
             protected Map<String, String> getParams() {
@@ -209,7 +177,7 @@ public class RegisterActivity extends AppCompatActivity {
         serverQueue.add(request);
     }
 
-    public static boolean isValidEmail(String email) {
-        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    public static boolean incorrectEmail(String email) {
+        return !Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 }
